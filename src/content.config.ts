3ee95a9a083cc36fa1dd8926dd31de24928920f7
Tyler Butler <tyler@tylerbutler.com>
2026-09-22 08:04:37 -0700

    fix(build): import zod directly

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,6 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
+import { z } from "zod";
 
 function uniqueIdGlob(options: Parameters<typeof glob>[0]): ReturnType<typeof glob> {
   const loader = glob(options);
